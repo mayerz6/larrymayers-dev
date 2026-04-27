95ec05a1360b47c547704f260059e81dd36a4f3d
Larry Mayers <l.mayerz@live.com>
2026-04-27 03:00:37 +0000

    fix(RemoveElemet+CSSAdjust): Slight touch up to footer & contact form design styles

--- views/partials/header.php
+++ views/partials/header.php
@@ -11,8 +11,8 @@ $rootDir = '/'; // Adjust this if your app is in a subdirectory
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Larry Mayers | Professional Portfolio Platform</title>
-        <link rel="stylesheet" href="<?= $rootDir ?>assets/css/style.css">
         <link rel="stylesheet" href="<?= $rootDir ?>assets/css/main.css">
+        <link rel="stylesheet" href="<?= $rootDir ?>assets/css/style.css">
         <link rel="icon" href="<?= $rootDir ?>assets/favicon.ico" type="image/x-icon">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
